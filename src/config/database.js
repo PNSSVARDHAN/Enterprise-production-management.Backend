f901fdfb7273671c20702190f48907cbe49229fd
PNSSVARDHAN <srivardhan0005@gmail.com>
2025-04-18 18:04:37 +0530

    updated data base config

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,24 @@
 
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
 
-// Create a new Sequelize instance
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    logging: false,  // Disable SQL query logging for cleaner output
-});
+// // Create a new Sequelize instance
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     port: process.env.DB_PORT,
+//     logging: false,  // Disable SQL query logging for cleaner output
+// });
 
-// Test database connection
-sequelize.authenticate()
-    .then(() => console.log("✅ Database Connected Successfully"))
-    .catch((err) => console.error("❌ Database Connection Failed:", err));
+// // Test database connection
+// sequelize.authenticate()
+//     .then(() => console.log("✅ Database Connected Successfully"))
+//     .catch((err) => console.error("❌ Database Connection Failed:", err));
 
-module.exports = sequelize; 
+// module.exports = sequelize; 
 
 
-/**
+
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
@@ -41,5 +41,5 @@ sequelize.authenticate()
     .then(() => console.log("✅ Database Connected Successfully"))
     .catch((err) => console.error("❌ Database Connection Failed:", err));
 
-module.exports = sequelize;  **/
+module.exports = sequelize; 
 
